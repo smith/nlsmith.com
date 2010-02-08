ce3568f58b7eb2301851226f0214c7a606453afe
smith <nlloyds@gmail.com>
2010-02-07 22:48:37 -0600

    recipe reorg

--- site.js
+++
@@ -1,7 +0,0 @@
-// Attempt to load the private site script
-try {
-    Jaxer.load("file://" + Jaxer.request.documentRoot + 
-        "jaxer-include/js/application.js");
-} catch (e) { 
-    Jaxer.Log.warn(e);
-}
