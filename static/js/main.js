e87331502a064c027b2990723c46e97a0c457fbc
Nathan L Smith <nlloyds@gmail.com>
2010-02-25 23:33:06 -0600

    links

---
+++ static/js/main.js
@@ -0,0 +1,31 @@
+// Require based on the page
+
+jQuery("document").ready(function ($) {
+
+// Get delicious links
+(function () {
+    var links = $("#links");
+
+    function li(o) {
+        return '<li><a href="' + o.u + '">' + o.d + '</a>' + o.n + '</li>';
+    }
+
+    $.getJSON(
+       "http://feeds.delicious.com/v2/json/nlsmith?count=25&callback=?",
+        function (data) { links.html($.map(data, li).join("")); }
+    );
+})();
+
+});
+
+// Google analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-1765938-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+    var ga = document.createElement('script');
+    ga.src = ('https:' == document.location.protocol ?
+        'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    ga.setAttribute('async', 'true');
+    document.documentElement.firstChild.appendChild(ga);
+})();
--- static/js/old/argo.js
+++ static/js/old/argo.js
@@ -10,53 +10,6 @@
 var argo = argo || {};
 
 /**
- * Reads a del.icio.us JSON feed 
- */
-argo.DeliciousFeed = Class.create(
-/** @scope argo.DeliciousFeed.prototype */
-{
-  /** @constructs */
-  initialize : function (options) {
-    // Set defaults and extend them with the given object argument
-    Object.extend(this, {
-      count : 25,
-      user : "",
-      feed : "http://feeds.delicious.com/feeds/json/"
-    });
-    Object.extend(this, options || {});
-
-    var url = this.feed + this.user + "?count=" + this.count;
-    this.posts = [];
-
-    // Make request for list
-    eval(Jaxer.Web.get(url)); 
-
-    try { 
-      if (Delicious && Delicious.posts) { this.posts = Delicious.posts; }
-      else { throw new Error(); }
-    } catch (e) {
-        Jaxer.Log.warn("Could not load Delicious posts");
-    }
-  },
-
-  /**
-   * Generate unordered HTML list items
-   */
-  toHTML : function () {
-    var listItems = "";
-    var template = '<li><a href="#{u}">#{d}</a>#{n}</li>\n';
-
-    this.posts.each(function (post) {
-      if(post.n) { post.n = '<p>' + post.n + '</p>'; }
-      listItems += template.interpolate(post); 
-    });
-
-    this.HTML = listItems;
-    return listItems;
-  }
-});
-
-/**
 * @class MailMessage For sending mail messages through SMTP. 
 * Uses the Jaxer.SMTP.Message object.
 */
