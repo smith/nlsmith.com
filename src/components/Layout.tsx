2e7886e5eb077b736091bf411799a000f4bff536
Nathan L Smith <smith@nlsmith.com>
2019-08-14 09:58:26 -0500

    Some layout theming

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,78 +1,76 @@
 import "./style.css";
 
-import { CommandBar, Link, Stack, Text } from "office-ui-fabric-react";
 import React, { FunctionComponent } from "react";
 
-import { Customizer } from "office-ui-fabric-react";
-import { FluentCustomizations } from "@uifabric/fluent-theme";
-import { Link as GatsbyLink } from "gatsby";
-import Helmet from "react-helmet";
+import { Link } from "gatsby";
+import { Stack } from "office-ui-fabric-react";
+import styled from "styled-components";
 
 interface LayoutProps {
   title: string;
 }
 
-export const H1: React.FunctionComponent = ({ children }) => {
-  return (
-    <Text as="h1" variant="xLarge">
-      {children}
-    </Text>
-  );
-};
+const H1 = styled("h1")`
+  display: inline;
+  font-weight: inherit;
+  margin: 0;
 
-const P: React.FunctionComponent = ({ children }) => {
-  return (
-    <Text as="p" block={true} styles={{ root: { textAlign: "center" } }}>
-      {children}
-    </Text>
-  );
-};
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+const UL = styled("ul")`
+  display: inline;
+  list-style: none;
+  margin: 0;
+
+  li {
+    display: inline;
+    margin-right: 1em;
+  }
+
+  li a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+const Footer = styled("footer")`
+  display: block;
+  margin-top: 2em;
+  text-align: center;
+  width: 100%;
+`;
 
 export const Layout: FunctionComponent<LayoutProps> = ({ children, title }) => {
   return (
-    <Customizer {...FluentCustomizations}>
-      <Helmet>
-        <link
-          rel="stylesheet"
-          href="https://static2.sharepointonline.com/files/fabric/office-ui-fabric-core/10.0.0/css/fabric.min.css"
-        />
-      </Helmet>
-
+    <>
       <Stack horizontal={true} as="header" horizontalAlign={"space-between"}>
         <H1>
-          <Link
-            as={GatsbyLink}
-            styles={{ root: { color: "inherit", textDecoration: "none" } }}
-            to="/"
-          >
-            {title}
-          </Link>
+          <Link to="/">{title}</Link>
         </H1>
-        <CommandBar
-          items={[]}
-          farItems={[
-            { key: "GitHub", name: "GitHub", href: "https://github.com/smith" },
-            {
-              key: "Twitter",
-              name: "Twitter",
-              href: "https://twitter.com/nlsmith"
-            },
-            { key: "Résumé", name: "Résumé", href: "/resume" },
-            {
-              key: "Contact",
-              name: "Contact",
-              href: "mailto:smith@nlsmith.com"
-            }
-          ]}
-        />
+        <UL>
+          <li>
+            <a href="https://github.com/smith">GitHub</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/nlsmith">Twitter</a>
+          </li>
+          <li>
+            <a href="/resume">Résumé</a>
+          </li>
+          <li>
+            <a href="mailto:smith@nlsmith.com">Contact</a>
+          </li>
+        </UL>
       </Stack>
-      <main>
-        <Stack horizontalAlign="center">{children}</Stack>
-      </main>
-      <footer style={{ bottom: 0, width: "100%" }}>
-        <P>© 2004 – {new Date().getFullYear()} Nathan Lloyd Smith</P>
-      </footer>
-    </Customizer>
+      <Stack as="main" horizontalAlign="center">
+        {children}
+      </Stack>
+      <Footer>© 2004 – {new Date().getFullYear()} Nathan Lloyd Smith</Footer>
+    </>
   );
 };
 
